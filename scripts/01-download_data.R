93f6b4eea87b37693916eea8fa96d93f926296e9
YiZhuo Li <52457749+eason1218@users.noreply.github.com>
2024-09-19 17:27:24 -0400

    Update 01-download_data.R

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,8 +1,8 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Download data of marriage in covid
+# Author: YiZhuo Li
+# Date: 19 Sep 2024
+# Contact: liyizhuo.li@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: [...UPDATE THIS...]
 # Any other information needed? [...UPDATE THIS...]
@@ -16,11 +16,16 @@ library(tidyverse)
 #### Download data ####
 # [...ADD CODE HERE TO DOWNLOAD...]
 
+package <- show_package
 
-
+resources <- list_package_resources
 #### Save data ####
 # [...UPDATE THIS...]
 # change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+datastore_resources <- filter(resources, tolower(format))
+
+data <- filter(datastore_resources, row_number()==1) %>%
+
+write_csv(data, "data/raw_data/raw_data.csv") 
 
          
